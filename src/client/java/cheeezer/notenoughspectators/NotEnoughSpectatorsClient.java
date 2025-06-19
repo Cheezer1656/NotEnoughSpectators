7d35c36cbc79635263a1c642c3f01aaf63a2be05
Cheezer1656 <118921295+Cheezer1656@users.noreply.github.com>
2025-06-18 17:40:46 -0700

    Initial commit

---
+++ src/client/java/cheeezer/notenoughspectators/NotEnoughSpectatorsClient.java
@@ -0,0 +1,10 @@
+package cheeezer.notenoughspectators;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NotEnoughSpectatorsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
