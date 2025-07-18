09edf1b34dffbec279ea9d73d978810cb9197e72
Cheezer1656 <118921295+Cheezer1656@users.noreply.github.com>
2025-07-18 13:36:00 -0700

    Remove automatic server starting, allow server to be stopped

--- src/client/java/cheeezer/notenoughspectators/NotEnoughSpectatorsClient.java
+++ src/client/java/cheeezer/notenoughspectators/NotEnoughSpectatorsClient.java
@@ -7,25 +7,49 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallba
 import net.minecraft.text.Text;
 
 public class NotEnoughSpectatorsClient implements ClientModInitializer {
+	private SpectatorServer server;
+
 	@Override
 	public void onInitializeClient() {
-		// Start the spectator server ASAP for testing purposes
-		startServer();
-
-		// Actual start server command (to be used in the future)
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("share")
 				.executes(context -> {
+					if (server != null) {
+						context.getSource().sendError(Text.literal("A server is already running!"));
+						return 0;
+					}
 					context.getSource().sendFeedback(Text.literal("Starting server..."));
 							startServer();
 							context.getSource().sendFeedback(Text.literal("Server started!"));
 							return 1;
 						}
 				)));
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("stop_sharing")
+				.executes(context -> {
+					if (server == null) {
+						context.getSource().sendError(Text.literal("No server is currently running!"));
+						return 0;
+					}
+					context.getSource().sendFeedback(Text.literal("Stopping server..."));
+							stopServer();
+							context.getSource().sendFeedback(Text.literal("Server stopped!"));
+							return 1;
+						}
+				)));
 	}
 
 	private void startServer() {
-		SpectatorServer server = new SpectatorServer(25565);
-		server.setDaemon(true);
-		server.start();
+		if (server == null) {
+			server = new SpectatorServer(25565);
+			server.setDaemon(true);
+			server.start();
+		}
+	}
+
+	private void stopServer() {
+		if (server != null) {
+			server.interrupt();
+			server = null;
+		}
 	}
 }
