743be04bd40bccb84ceff9144ce5739f32aff06d
Cheezer1656 <118921295+Cheezer1656@users.noreply.github.com>
2025-07-18 17:32:19 -0700

    Update mod info and rename main class

--- src/main/java/cheezer/notenoughspectators/NotEnoughSpectators.java
+++ src/main/java/cheezer/notenoughspectators/NotEnoughSpectators.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = "notenoughspectators", useMetadata=true)
-public class ExampleMod {
+public class NotEnoughSpectators {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
